d17f63102394ca7528c10b92964bf43c2307fe78
Bryan Gomez <bgomez718@gmail.com>
2024-07-26 16:36:42 -0400

    created hero landing page in mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': '#2DC0C9', // Custom teal color
+      },
+      backgroundImage: {
+        'white-to-transparent': 'linear-gradient(to top,rgba(255, 255, 255, .25), rgba(160, 160, 160, .05))',
+      },
+    },
   },
   plugins: [],
-} 
-
+}
