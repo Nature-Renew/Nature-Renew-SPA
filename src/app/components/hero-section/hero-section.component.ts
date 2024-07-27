76f332067627404b0366e317d2b27bfab0659ee8
Gregory Vincent Jr <gregvjrr@gmail.com>
2024-07-27 18:15:08 -0400

    Added Angular Material componnet. Work in progress.

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { InfoComponentComponent } from '../info-component/info-component.component';
 // import down_arrow from '../../assets/general_svgs/down_arrow.svg';
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [],
+  imports: [InfoComponentComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css'
 })
+ 
 export class HeroSectionComponent {
+  isVisible = true;
+  onScroll(){
+    this.isVisible = false;
+  }
 
 }
