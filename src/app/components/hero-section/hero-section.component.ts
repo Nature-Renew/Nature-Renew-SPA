e60b620feee0f0568b0991d0f2a7ef7f72edd701
Gregory Vincent Jr <gregvjrr@gmail.com>
2024-07-27 10:55:02 -0400

    Working on NatureRenew.

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+// import down_arrow from '../../assets/general_svgs/down_arrow.svg';
 @Component({
   selector: 'app-hero-section',
   standalone: true,
