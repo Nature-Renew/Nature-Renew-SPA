deb505da01f056554a212f49d386a0a667fd0463
Gregory Vincent Jr <gregvjrr@gmail.com>
2024-07-26 20:50:26 -0400

    Slight tweaks to existing PR.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       colors: {
         'primary-color': '#2DC0C9', // Custom teal color
+        'primary-yellow': '#DAB634', // taken from figma file
       },
       backgroundImage: {
         'white-to-transparent': 'linear-gradient(to top,rgba(255, 255, 255, .25), rgba(160, 160, 160, .05))',
