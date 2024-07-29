b241d074dcacb5b04312d25579f5114952671b44
Bryan Gomez <bgomez718@gmail.com>
2024-07-29 18:36:33 -0400

    Created Newletter form with validation

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,  NgStyle],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css'
 })
 export class HeroSectionComponent {
 
+
+  isSubmitted: boolean = false;
+
+  newsLetterForm = new FormGroup({
+    name : new FormControl('',[Validators.required, Validators.maxLength(50)]),
+    email : new FormControl('',[Validators.required, Validators.email])
+  });
+
+  onReset(){
+    this.isSubmitted = false;
+    this.newsLetterForm.reset();
+  }
+
+  onSubmit(){
+
+    this.isSubmitted = true;
+
+    if (this.newsLetterForm.valid) {
+      console.log(this.newsLetterForm.value);
+      
+    } else {
+      console.warn('Form is invalid');
+      console.log(this.newsLetterForm.hasError.toString)
+     
+    }
+  }
+
+  get name(){
+    console.log("first name value: " )
+    return this.newsLetterForm.get('name');
+  }
+
+
+  get email(){
+    return this.newsLetterForm.get('email');
+  }
+
+  
+
+
+  constructor(){
+
+  }
+  
 }
+  
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       colors: {
         'primary-color': '#2DC0C9', // Custom teal color
+        'primary-color-600': '#218c91', // Custom teal color
       },
       backgroundImage: {
         'white-to-transparent': 'linear-gradient(to top,rgba(255, 255, 255, .25), rgba(160, 160, 160, .05))',
