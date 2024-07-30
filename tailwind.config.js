efde338aaa75dc4205cacad36c84af74465f18b4
Gregory Vincent Jr <gregvjrr@gmail.com>
2024-07-30 19:29:20 -0400

    Added different responsive breakpoints.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
       backgroundImage: {
         'white-to-transparent': 'linear-gradient(to top,rgba(255, 255, 255, .25), rgba(160, 160, 160, .05))',
       },
+      screens: {
+        'mobile_view': '435px', // min width of 435px
+        'tablet_view': '700px', // min width of 700px
+        'large_tablet_view': '1000px', // min width of 1000px
+      }
     },
   },
   plugins: [],
