67c5dcf234a57ba1add0afe1865d4ef3e9f91b90
Gregory Vincent Jr <gregvjrr@gmail.com>
2024-08-05 17:45:25 -0400

    Updated background image.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary-color': '#90EE90', // Custom teal color
+        'primary-color': '#90EE90', // Custom green color
         'primary-yellow': '#DAB634', // taken from figma file
-        'primary-color-600': '#218c91', // Custom teal color
+        'primary-color-600': '#AFE1AF', // Custom green color 2 
       },
       backgroundImage: {
         'white-to-transparent': 'linear-gradient(to top,rgba(255, 255, 255, .25), rgba(160, 160, 160, .05))',
