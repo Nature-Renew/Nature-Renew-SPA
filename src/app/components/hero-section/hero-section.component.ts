2bf046485b8e7222fff23ce40ca21e1a36b19841
Gregory Vincent Jr <gregvjrr@gmail.com>
2024-10-15 20:41:15 -0400

    working with api gateway.

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -5,7 +5,7 @@ import { NgStyle } from '@angular/common';
 import { InfoComponentComponent } from '../info-component/info-component.component';
 import {HttpClient} from '@angular/common/http';
 //@ts-ignore
-import environment from '../../../environments/environment.prod.js';
+import environment from '../../../environments/environment.prod.ts';
 
 @Component({
   selector: 'app-hero-section',
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 const environment = {
-    TABLE_NAME: process.env['TABLE_NAME'],
-    REGION: process.env['REGION'],
-    API_GATEWAY_ENDPOINT: process.env['API_GATEWAY_ENDPOINT'],
+    API_GATEWAY_ENDPOINT: 'https://md93dvyv1d.execute-api.us-east-1.amazonaws.com/Prod',
 };
 
 export default environment;
