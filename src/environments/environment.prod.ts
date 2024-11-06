a059873599d15e3eccff3ad4e9d2096137204280
Gregory Vincent Jr <gregvjrr@gmail.com>
2024-11-06 10:51:13 -0500

    Testing api access.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 const environment = {
-    API_GATEWAY_ENDPOINT: "https://dzdcejqhwg.execute-api.us-east-1.amazonaws.com/production",
+    API_GATEWAY_ENDPOINT: "https://dzdcejqhwg.execute-api.us-east-1.amazonaws.com",
 };
 
 export default environment;
