c5696f1616d0dc8e9a00ccc508bc31cd69e99488
Gregory Vincent Jr <gregvjrr@gmail.com>
2024-11-07 12:44:10 -0500

    Working on api gateway. switching endpoint url.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 const environment = {
-    API_GATEWAY_ENDPOINT: "https://dzdcejqhwg.execute-api.us-east-1.amazonaws.com",
+    API_GATEWAY_ENDPOINT: "https://dzdcejqhwg.execute-api.us-east-1.amazonaws.com/production",
 };
 
 export default environment;
